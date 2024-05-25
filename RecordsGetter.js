45fa952f5b33f88c241696fabd8747eda2bfa839
AbyssPortal <planteraminehut@gmail.com>
2024-05-25 22:22:58 +0300

    can now select the maximum amount of users sampled per rank. users are chosen randomly from the rank.

--- RecordsGetter.js
+++ RecordsGetter.js
@@ -13,20 +13,10 @@ async function main() {
     const saveFrequency = 100;
     let recordUserArray = [];
     let usersAlreadyDone = [];
+    let maxUsersPerRank = 200;
 
-    let input;
 
-    console.log('Do you want to use unfinished data? (y/n)')
-    const userInputPromise = new Promise(resolve => {
-        process.stdin.once('data', data => {
-            input = data.toString().trim(); // get the user input and remove trailing whitespace
-            resolve(input); // resolve the promise with the user input
-        });
-    });
-
-    await userInputPromise
-
-    if (input === 'y') {
+    if (await yesOrNo("use previous data? (y/n)")) {
 
         try {
             const jsonString = await fs.promises.readFile('metaResults.json', 'utf8');
@@ -70,51 +60,40 @@ async function main() {
             }
         });
     }
-
+    let rank_arrays = {};
     try {
         console.log("getting users...");
         let users = await getUsers();
         const len = users.length;
         for (i = 0; i < len; i++) {
-            console.log(`${chalk.blueBright(users[i].username)}: ${chalk.yellow(`${users[i].league.rating} TR`)}, ` +
-                `${chalk.red(`${users[i].league.rank.toUpperCase()}`)}\x1b[0m`);
-            maxTr = users[i].league.rating;
+
+            if (!(users[i].league.rank in rank_arrays)) {
+                rank_arrays[users[i].league.rank] = [];
+            }
+
             if (usersAlreadyDone.includes(users[i].username)) {
                 continue;
-            } else {
-                usersAlreadyDone.push(users[i].username);
             }
-
-            let getRecordPromise = new Promise(resolve => {
-                getUserRecords(users[i].username).then(records => {
-                    recordUserArray.push({tetraStats: users[i], records: records})
-                    resolve();
-                })
-            })
-
-            //wait for user input or 1 second whichever comes first
-            const timerPromise = new Promise(resolve => setTimeout(resolve, 1000));
-            let stopRequested = false;
-            const userInputPromise = new Promise(resolve => {
-                process.stdin.once('data', () => {
-                    stopRequested = true;
-                    resolve();
-                });
-            });
-
-            await Promise.race([Promise.all([timerPromise, getRecordPromise]), userInputPromise]);
-
-            if (stopRequested) {
-                console.log("User has requested to stop.");
-
-                saveDataMidway(recordUserArray);
-
-                return;
-            }
+            rank_arrays[users[i].league.rank].push(users[i]);
 
 
-            if (i % saveFrequency === 0 && i !== 0) {
-                saveDataMidway(recordUserArray);
+        }
+        for (let rank in rank_arrays) {
+            let arr = rank_arrays[rank]
+            // console.log(arr);
+
+            // Shuffle array
+            const shuffled = arr.sort(() => 0.5 - Math.random());
+
+        // Get sub-array of first n elements after shuffled
+            let selected = shuffled.slice(0, maxUsersPerRank);
+            let i = 0;
+            for (let user of selected) {
+                await addUserRecords(user);
+                i++;
+                if (i%saveFrequency == 0) {
+                    saveDataMidway(recordUserArray);
+                }
             }
         }
         fs.writeFile('results.json', JSON.stringify(recordUserArray), (error) => {
@@ -128,10 +107,58 @@ async function main() {
         console.error(error);
     }
 
+    async function addUserRecords(user) {
+        console.log(`${chalk.blueBright(user.username)}: ${chalk.yellow(`${user.league.rating} TR`)}, ` +
+            `${chalk.red(`${user.league.rank.toUpperCase()}`)}\x1b[0m`);
+        maxTr = user.league.rating;
+
+        let getRecordPromise = new Promise(resolve => {
+            getUserRecords(user.username).then(records => {
+                recordUserArray.push({tetraStats: user, records: records})
+                resolve();
+            })
+        })
+
+        //wait for user input or 1 second whichever comes first
+        const timerPromise = new Promise(resolve => setTimeout(resolve, 1000));
+        let stopRequested = false;
+        const userInputPromise = new Promise(resolve => {
+            process.stdin.once('data', () => {
+                stopRequested = true;
+                resolve();
+            });
+        });
+
+        await Promise.race([Promise.all([timerPromise, getRecordPromise]), userInputPromise]);
+
+        if (stopRequested) {
+            console.log("User has requested to stop.");
+
+            saveDataMidway(recordUserArray);
+
+            return;
+        }
+
+        usersAlreadyDone.push(user.username);
+    }
 
 }
 
+async function yesOrNo(question) {
+    let input;
 
+    console.log(question)
+    const userInputPromise = new Promise(resolve => {
+        process.stdin.once('data', data => {
+            input = data.toString().trim(); // get the user input and remove trailing whitespace
+            resolve(input); // resolve the promise with the user input
+        });
+    });
+
+    await userInputPromise;
+
+    return input.startsWith("y");
+}
 async function getUsersInRange(limit, after) {
     let url = `https://ch.tetr.io/api/users/lists/league?limit=${limit}&after=${after}`;
     let response = await fetch(url);
